2baf17dff7a3570c707498596996ffc8bf6dd8bb
unknown <rafik.towati@gmail.com>
2024-12-14 21:11:06 +0300

    bug fix + some contents

--- afw_my_files.php
+++ afw_my_files.php
@@ -1,5 +1,5 @@
 <?php
-$file_dir_name = dirname(__FILE__); 
-//die("$file_dir_name/../lib/afw/afw_my_files.php");
-include("$file_dir_name/../lib/afw/afw_my_files.php");
+$dir_name = dirname(__FILE__); 
+$My_Module = "crm";
+include("$dir_name/../lib/afw/afw_my_files.php");
 ?>
