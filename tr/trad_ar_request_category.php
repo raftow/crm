075d6e941f411067c9a6a2b3a550e8e90d565f72
raftow <rboubaker@tvtc.gov.sa>
2026-09-09 13:42:48 +0300

    work on workflow - etl - crm - store - projects

---
+++ tr/trad_ar_request_category.php
@@ -0,0 +1,9 @@
+<?php
+	$trad["request_category"]["requestcategory.single"] = "صنف طلب الكتروني";
+	$trad["request_category"]["requestcategory.new"] = "جديد";
+	$trad["request_category"]["request_category"] = "أصناف الطلبات الالكترونية";
+	$trad["request_category"]["name_ar"] = "المسمى";
+	$trad["request_category"]["name_en"] = "المسمى بالانجليزي";
+	$trad["request_category"]["request_type_mfk"] = "أنواع الطلبات الالكترونية";
+	$trad["request_category"]["request_type_mfk_help"] = "أنواع الطلبات الالكترونية التي لها علاقة بهذا الصنف من الطلبات";
+?>
---
+++ tr/trad_en_request_category.php
@@ -0,0 +1,9 @@
+<?php
+	$trad["request_category"]["requestcategory.single"] = "Request Category";
+	$trad["request_category"]["requestcategory.new"] = "New";
+	$trad["request_category"]["request_category"] = "Request Categories";
+	$trad["request_category"]["name_ar"] = "Name (Arabic)";
+	$trad["request_category"]["name_en"] = "Name (English)";
+	$trad["request_category"]["request_type_mfk"] = "Request Types";
+	$trad["request_category"]["request_type_mfk_help"] = "Request types associated with this category";
+?>
