e4c9fd161f1fbabf14094891e4f8b1eecc09aed5
raftow <rboubaker@tvtc.gov.sa>
2026-09-10 13:07:13 +0300

    work on workflow - etl - crm - store - projects

--- index.php
+++ index.php
@@ -11,7 +11,7 @@ if ($objme) {
                         if (file_exists(dirname(__FILE__)."/../client-$company/organization_business.php")) {
                                 require_once(dirname(__FILE__)."/../client-$company/organization_business.php");
                                 if (class_exists('OrganizationBusiness')) {
-                                        $res = OrganizationBusiness::update_all_organizations();
+                                        $res = OrganizationBusiness::update_all_organizations(true, $_GET["case"]);
                                         $warning = implode("<br>\n", $res['warnings']);
                                         $error = implode("<br>\n", $res['errors']);
                                         $info = implode("<br>\n", $res['log']);
