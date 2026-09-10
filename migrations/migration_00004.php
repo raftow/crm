95409be43a13512384f72eb9c13d67bb878ce705
raftow <rboubaker@tvtc.gov.sa>
2026-09-10 17:16:37 +0300

    work on workflow - etl - crm - store - projects

--- migrations/migration_00004.php
+++ migrations/migration_00004.php
@@ -5,7 +5,9 @@ if (!class_exists("AfwSession")) die("Denied access");
  */
 $server_db_prefix = AfwSession::currentDBPrefix();
 try {
-    AfwDatabase::db_query("ALTER TABLE " . $server_db_prefix . "crm.crm_orgunit add   id_responsible int(11) DEFAULT NULL  AFTER orgunit_id;");
+    AfwDatabase::db_query("ALTER TABLE " . $server_db_prefix . "hrm.orgunit add   id_responsible int(11) DEFAULT NULL;");
+    AfwDatabase::db_query("ALTER TABLE " . $server_db_prefix . "hrm.orgunit add   log text  DEFAULT NULL  AFTER id_responsible;");
+    AfwDatabase::db_query("ALTER TABLE " . $server_db_prefix . "crm.crm_orgunit add   id_responsible int(11) DEFAULT NULL  AFTER orgunit_id;");    
     AfwDatabase::db_query("UPDATE " . $server_db_prefix . "crm.crm_orgunit co set co.id_responsible = (select id_responsible from " . $server_db_prefix . "hrm.orgunit where id = co.orgunit_id)");
 
     AfwDatabase::db_query("DROP TABLE IF EXISTS " . $server_db_prefix . "crm.request_category;");
